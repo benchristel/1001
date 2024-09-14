b35f40cd1cb66a1a700f2bd8ffa0eba168cde47c
Ben Christel <ben.christel@gmail.com>
2024-09-14 16:17:00 -0700

    Green: plus

--- notes.md
+++ notes.md
@@ -2,3 +2,4 @@
 
 Red: Can't find variable: plus
 Red: expect(1, is, 3)
+Green: adds two numbers
--- src/plus.ts
+++ src/plus.ts
@@ -1,3 +1,3 @@
 export function plus(a: number, b: number): number {
-    return a
+    return a + b
 }

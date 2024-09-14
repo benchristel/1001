bbc45df32aa2100b5c0a51f1f28ec51d5d8a5216
Ben Christel <ben.christel@gmail.com>
2024-09-14 16:34:04 -0700

    Green: add overload signatures to plus

--- src/plus.ts
+++ src/plus.ts
@@ -1,3 +1,5 @@
-export function plus(a: number | string, b: number | string): number | string {
+export function plus(a: number, b: number): number
+export function plus(a: string, b: string): string
+export function plus(a: any, b: any) {
     return a + b
 }

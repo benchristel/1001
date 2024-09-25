fe3828ebe8f814855f3bc036e8b6c082d387afdc
Ben Christel <ben.christel@gmail.com>
2024-09-24 17:50:29 -0700

    Create add() and sum()

---
+++ src/index.test.ts
@@ -0,0 +1,33 @@
+import {test, expect, is} from "@benchristel/taste"
+
+test("add()", {
+    "adds two numbers"() {
+        const result = add(1, 2)
+        expect(result, is, 3)
+    },
+})
+
+function add(a: number, b: number): number {
+    return a + b
+}
+
+test("sum()", {
+    "adds up an array of numbers"() {
+        const result = sum([1, 2, 5])
+        expect(result, is, 8)
+    },
+
+    "returns a lone number"() {
+        const result = sum([1])
+        expect(result, is, 1)
+    },
+
+    "returns 0 given empty array"() {
+        const result = sum([])
+        expect(result, is, 0)
+    },
+})
+
+function sum(numbers: number[]): number {
+    return numbers.reduce(add, 0)
+}

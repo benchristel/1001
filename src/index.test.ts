49dfbd029266eb284ce10b6a3ff911027f59ff8a
Ben Christel <ben.christel@gmail.com>
2024-10-03 20:54:31 -0700

    Backfill tests for isPlainObject

--- src/index.test.ts
+++ src/index.test.ts
@@ -290,12 +290,28 @@ test("isPlainObject()", {
         expect(isPlainObject(true), is, false)
     },
 
+    "is true for a plain object with its own constructor property"() {
+        const object = {constructor: "bork"}
+        expect(isPlainObject(object), is, true)
+    },
+
     "narrows types"() {
         let x: unknown = {}
         // @ts-expect-error
         "foo" in x
         if (isPlainObject(x)) {
             "foo" in x
+        }
+    },
+
+    "types properties as unknown"() {
+        // Ensures that the return type of isPlainObject is not just
+        // `x is Object`, which would not allow property access.
+        let x: unknown = {}
+        if (isPlainObject(x)) {
+            x.foo satisfies unknown
+            // @ts-expect-error
+            x.foo satisfies string
         }
     },
 })

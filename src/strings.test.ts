9b7c1fc8ced89f6b0a4499df34c80e71102d9a27
Ben Christel <ben.christel@gmail.com>
2024-12-25 14:28:36 -0700

    Inline variables in tests

--- src/strings.test.ts
+++ src/strings.test.ts
@@ -3,18 +3,10 @@ import {indicesOf} from "./strings.js"
 
 test("indicesOf", {
     "finds an empty string in an empty string"() {
-        const needle = ""
-        const haystack = ""
-        const expected: number[] = [0]
-        const result = [...indicesOf(needle, haystack)]
-        expect(result, equals, expected)
+        expect([...indicesOf("", "")], equals, [0])
     },
 
     "finds an empty string at each position"() {
-        const needle = ""
-        const haystack = "123"
-        const expected: number[] = [0, 1, 2, 3]
-        const result = [...indicesOf(needle, haystack)]
-        expect(result, equals, expected)
+        expect([...indicesOf("", "123")], equals, [0, 1, 2, 3])
     },
 })

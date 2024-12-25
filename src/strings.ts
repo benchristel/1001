658009d44ce465d9420a90a6c7bd413a1c2cb43d
Ben Christel <ben.christel@gmail.com>
2024-12-25 14:57:16 -0700

    indicesOf: JSDoc

--- src/strings.ts
+++ src/strings.ts
@@ -1,5 +1,11 @@
 const NOT_FOUND_INDEX = -1
 
+/**
+ * Finds occurrences of `needle` in `haystack`.
+ *
+ * @yields each index in `haystack` at which an occurrence of `needle` starts,
+ * in ascending order. Occurrences can overlap.
+ */
 export function *indicesOf(needle: string, haystack: string) {
     for (let i = 0; i <= haystack.length; i++) {
         i = haystack.indexOf(needle, i)

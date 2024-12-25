79cff3d08e212013cc8a8d8f4a6154021681eade
Ben Christel <ben.christel@gmail.com>
2024-12-25 15:34:14 -0700

    Add nonOverlappingIndicesOf

--- src/strings.test.ts
+++ src/strings.test.ts
@@ -1,5 +1,5 @@
 import {test, expect, equals} from "@benchristel/taste"
-import {indicesOf} from "./strings.js"
+import {indicesOf, nonOverlappingIndicesOf} from "./strings.js"
 
 test("indicesOf", {
     "finds an empty string in an empty string"() {
@@ -22,5 +22,39 @@ test("indicesOf", {
         const needle = "cal"
         const haystack = "Electrical calculator"
         expect([...indicesOf(needle, haystack)], equals, [7, 11])
+    },
+})
+
+test("nonOverlappingIndicesOf()", {
+    "finds an empty string in an empty string"() {
+        expect([...nonOverlappingIndicesOf("", "")], equals, [0])
+    },
+
+    "finds an empty string at each position"() {
+        expect([...nonOverlappingIndicesOf("", "123")], equals, [0, 1, 2, 3])
+    },
+
+    "returns an empty array if `needle` is not in `haystack`"() {
+        expect([...nonOverlappingIndicesOf("a", "")], equals, [])
+    },
+
+    "doesn't find overlapping occurrences of `needle`"() {
+        expect([...nonOverlappingIndicesOf("aaa", "aaaa")], equals, [0])
+    },
+
+    "finds repeated occurrences of a one-character string"() {
+        expect([...nonOverlappingIndicesOf("a", "aaa")], equals, [0, 1, 2])
+    },
+
+    "finds non-overlapping occurrences of a multi-character string"() {
+        const needle = "abc"
+        const haystack = "abcabc"
+        expect([...nonOverlappingIndicesOf(needle, haystack)], equals, [0, 3])
+    },
+
+    "finds non-adjacent non-overlapping occurrences"() {
+        const needle = "abc"
+        const haystack = "-abc-abc-"
+        expect([...nonOverlappingIndicesOf(needle, haystack)], equals, [1, 5])
     },
 })
--- src/strings.ts
+++ src/strings.ts
@@ -15,3 +15,21 @@ export function *indicesOf(needle: string, haystack: string) {
         yield i
     }
 }
+
+/**
+ * Finds non-overlapping occurrences of `needle` in `haystack`.
+ *
+ * @yields each index in `haystack` at which an occurrence of `needle` starts,
+ * in ascending order. Overlapping occurrences (those that share characters
+ * with a previous occurrence) are skipped.
+ */
+export function *nonOverlappingIndicesOf(needle: string, haystack: string) {
+    const stride = needle.length || 1
+    for (let i = 0; i <= haystack.length; i += stride) {
+        i = haystack.indexOf(needle, i)
+        if (i === NOT_FOUND_INDEX) {
+            break
+        }
+        yield i
+    }
+}

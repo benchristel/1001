0650a687aa69f0fa3777bc419516b9b8e25ef17e
Ben Christel <ben.christel@gmail.com>
2024-12-27 17:07:19 -0700

    Move isArray to new file arrays.ts

---
+++ src/arrays.test.ts
@@ -0,0 +1,38 @@
+import {test, expect, is} from "@benchristel/taste"
+import {isArray} from "./arrays.js"
+
+test("isArray()", {
+    "is true for an array"() {
+        expect(isArray([]), is, true)
+    },
+
+    "is false for a string"() {
+        expect(isArray(""), is, false)
+    },
+
+    "is false for an object"() {
+        expect(isArray({}), is, false)
+    },
+
+    "is false for `arguments`"() {
+        expect(isArray(arguments), is, false)
+    },
+
+    "is false for a typed array"() {
+        expect(isArray(new Uint8Array()), is, false)
+    },
+
+    "is false for null"() {
+        expect(isArray(null), is, false)
+    },
+
+    "narrows types"() {
+        let x: unknown = []
+        // @ts-expect-error
+        x[0]
+        if (isArray(x)) {
+            // no type error here
+            x[0]
+        }
+    },
+})

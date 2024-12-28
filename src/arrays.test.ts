7d8f7ea8bb0f1983f7853e34532eff01908e2d4c
Ben Christel <ben.christel@gmail.com>
2024-12-27 21:13:19 -0700

    Add filter function

--- src/arrays.test.ts
+++ src/arrays.test.ts
@@ -1,5 +1,6 @@
 import {test, expect, is, equals} from "@benchristel/taste"
-import {isArray, map} from "./arrays.js"
+import {filter, isArray, map} from "./arrays.js"
+import {_} from "./composition.js"
 
 test("isArray()", {
     "is true for an array"() {
@@ -45,5 +46,33 @@ test("map", {
 
     "does not pass the array index to the callback"() {
         expect(map(parseInt)(["10", "10", "10"]), equals, [10, 10, 10])
+    },
+})
+
+test("filter", {
+    "keeps only elements for which the predicate returns true"() {
+        const isEven = (n: number) => n % 2 === 0
+        expect(_([0, 1, 2, 3], filter(isEven)), equals, [0, 2])
+    },
+
+    "treats truthy values as true and falsey values as false"() {
+        const isOdd = (n: number) => n % 2
+        expect(_([0, 1, 2, 3], filter(isOdd)), equals, [1, 3])
+    },
+
+    "has the correct return type"() {
+        const isOdd = (n: number) => n % 2
+        _([0, 1, 2, 3], filter(isOdd)) satisfies number[]
+
+        // @ts-expect-error
+        _([0, 1, 2, 3], filter(isOdd)) satisfies string[]
+    },
+
+    "narrows types given a type predicate function"() {
+        const isString = (x: unknown): x is string => typeof x === "string"
+        _(["a", 1, "b", 2], filter(isString)) satisfies string[]
+
+        // @ts-expect-error
+        _(["a", 1, "b", 2], filter(isString)) satisfies number[]
     },
 })
--- src/arrays.ts
+++ src/arrays.ts
@@ -5,3 +5,14 @@ export function map<T, U>(f: (elem: T) => U) {
         return array.map((elem) => f(elem))
     }
 }
+
+export function filter<
+    Narrowed,
+    Predicate extends (x: any) => x is Narrowed,
+>(predicate: Predicate): <Element extends Parameters<Predicate>[0]>(array: Element[]) => Narrowed[]
+export function filter<
+    Predicate extends (x: any) => unknown,
+>(predicate: Predicate): <Element extends Parameters<Predicate>[0]>(array: Element[]) => Element[]
+export function filter(predicate: any) {
+    return (array: any) => array.filter(predicate)
+}

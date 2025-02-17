ec0f6769d165f704d5b56f9e804817887bac0fd3
Ben Christel <ben.christel@gmail.com>
2025-02-17 11:48:15 -0700

    Rename parameters

--- src/composition.ts
+++ src/composition.ts
@@ -1,41 +1,41 @@
 /**
- * Pipes the given `value` through zero to five functions.
+ * Pipes the given `initialValue` through zero to five functions.
  *
  * @see startWith for longer pipelines.
  */
 export function _<Ret>(
-    value: Ret): Ret
+    initialValue: Ret): Ret
 export function _<A, Ret>(
-    value: A,
+    initialValue: A,
     f1: (a: A) => Ret): Ret
 export function _<A, B, Ret>(
-    value: A,
+    initialValue: A,
     f1: (a: A) => B,
     f2: (b: B) => Ret): Ret
 export function _<A, B, C, Ret>(
-    value: A,
+    initialValue: A,
     f1: (a: A) => B,
     f2: (b: B) => C,
     f3: (c: C) => Ret): Ret
 export function _<A, B, C, D, Ret>(
-    value: A,
+    initialValue: A,
     f1: (a: A) => B,
     f2: (b: B) => C,
     f3: (c: C) => D,
     f4: (d: D) => Ret): Ret
 export function _<A, B, C, D, E, Ret>(
-    value: A,
+    initialValue: A,
     f1: (a: A) => B,
     f2: (b: B) => C,
     f3: (c: C) => D,
     f4: (d: D) => E,
     f5: (e: E) => Ret): Ret
-export function _(value: any, ...functions: any[]) {
-    return functions.reduce((curr, f) => f(curr), value)
+export function _(initialValue: any, ...functions: any[]) {
+    return functions.reduce((x, f) => f(x), initialValue)
 }
 
-export function startWith<T>(value: T): Pipeline<T> {
-    return new Pipeline(value)
+export function startWith<T>(initialValue: T): Pipeline<T> {
+    return new Pipeline(initialValue)
 }
 
 class Pipeline<T> {

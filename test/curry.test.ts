0e65130dcbaa16f143c7e755e2ec6d802163b574
Ben Christel <ben.christel@gmail.com>
2025-03-14 09:57:19 -0700

    Remove code to support pretty-formatting from curry()

--- test/curry.test.ts
+++ test/curry.test.ts
@@ -11,12 +11,6 @@ test("curry", {
         const curriedIncrement = curry((n: number) => n + 1)
         expect(curriedIncrement(1), is, 2)
     },
-
-    "preserves the name of the original function"() {
-        function named() {}
-        const curried = curry(named)
-        expect(curried.displayName, is, "named")
-    },
 })
 
 const curriedConcat2 = curry((a: string, b: string) => a + b)
@@ -49,34 +43,5 @@ test("a curried three-argument function", {
     "can be passed its arguments in groups"() {
         expect(curriedAdd3(1, 2)(4), is, 7)
         expect(curriedAdd3(1)(3, 4), is, 8)
-    },
-})
-
-function add(a: number, b: number) {
-    return a + b
-}
-
-test("a partial application of a curried function", {
-    "retains the name of the original function"() {
-        const add1 = curry(add)(1)
-
-        expect(add1.displayName, is, "add")
-    },
-
-    "retains a displayName given to the curried function"() {
-        const curriedAdd = curry(add)
-        curriedAdd.displayName = "the displayName"
-        const add1 = curriedAdd(1)
-
-        expect(add1.displayName, is, "the displayName")
-    },
-
-    "retains a displayName given to the original function"() {
-        const add = (a: number, b: number) => a + b
-        add.displayName = "the displayName"
-
-        const add1 = curry(add)(1)
-
-        expect(add1.displayName, is, "the displayName")
     },
 })
